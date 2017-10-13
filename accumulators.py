959ec67bcaaf534a5bfa582fde7fb4f010c2803c
progvk3 <32221403+progvk3@users.noreply.github.com>
2017-10-13 23:35:05 +0530

    Hi

---
+++ accumulators.py
@@ -0,0 +1,14 @@
+rdd1 = sc.textFile("file:///home/vasanth/pyspark/docs/testfile1.txt")
+
+rdd2 = rdd1.flatMap(lambda x: x.split(" "))
+
+final = []
+
+for i in rdd2.collect():
+	a = sc.accumulator(0)
+	for j in rdd2.collect():
+		if i == j:
+			a +=1
+	final.append(i,a.value)
+
+print(final)
